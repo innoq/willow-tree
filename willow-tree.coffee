13e3406e4989b494d2b61770edff7179a08c9f28
Falk Hoppe <falkhoppe81@gmail.com>
2014-01-28 15:57:51 +0100

    removed unnecessary jquery wrapper

--- willow-tree.coffee
+++ willow-tree.coffee
@@ -1,28 +1,27 @@
-(($) ->
+$ = jQuery
 
-  class WillowTree
-    @init: =>
-      $context = $(@)
+class WillowTree
+  @init: =>
+    $context = $(@)
 
-      $context.off 'click.toggle.willow-tree'
-      $context.on 'click.toggle.willow-tree', '.js-willow-tree-toggle', @toggle
+    $context.off 'click.toggle.willow-tree'
+    $context.on 'click.toggle.willow-tree', '.js-willow-tree-toggle', @toggle
 
-    @toggle: (e) ->
-      e.preventDefault()
-      new WillowLeaf().toggle(e)
+  @toggle: (e) ->
+    e.preventDefault()
+    new WillowLeaf().toggle(e)
 
 
-  class WillowLeaf
-    toggle: (event) =>
-      target = event.target
-      $target = $(target)
+class WillowLeaf
+  toggle: (event) =>
+    target = event.target
+    $target = $(target)
 
-      $parentListElement = $target.parent('li')
-      $subList = $parentListElement.children('ul')
+    $parentListElement = $target.parent('li')
+    $subList = $parentListElement.children('ul')
 
-      $subList.toggle()
-      $target.toggleClass("toggled")
+    $subList.toggle()
+    $target.toggleClass("toggled")
 
-  $.fn.willowTree = WillowTree.init
+$.fn.willowTree = WillowTree.init
 
-)(jQuery)
--- willow-tree.js
+++ willow-tree.js
@@ -1,45 +1,47 @@
 (function() {
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var $, WillowLeaf, WillowTree,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-  (function($) {
-    var WillowLeaf, WillowTree;
-    WillowTree = (function() {
-      function WillowTree() {}
+  $ = jQuery;
 
-      WillowTree.init = function() {
-        var $context;
-        $context = $(WillowTree);
-        $context.off('click.toggle.willow-tree');
-        return $context.on('click.toggle.willow-tree', '.js-willow-tree-toggle', WillowTree.toggle);
-      };
+  WillowTree = (function() {
+    function WillowTree() {}
 
-      WillowTree.toggle = function(e) {
-        e.preventDefault();
-        return new WillowLeaf().toggle(e);
-      };
+    WillowTree.init = function() {
+      var $context;
+      $context = $(WillowTree);
+      $context.off('click.toggle.willow-tree');
+      return $context.on('click.toggle.willow-tree', '.js-willow-tree-toggle', WillowTree.toggle);
+    };
 
-      return WillowTree;
+    WillowTree.toggle = function(e) {
+      e.preventDefault();
+      return new WillowLeaf().toggle(e);
+    };
 
-    }).call(this);
-    WillowLeaf = (function() {
-      function WillowLeaf() {
-        this.toggle = __bind(this.toggle, this);
-      }
+    return WillowTree;
 
-      WillowLeaf.prototype.toggle = function(event) {
-        var $parentListElement, $subList, $target, target;
-        target = event.target;
-        $target = $(target);
-        $parentListElement = $target.parent('li');
-        $subList = $parentListElement.children('ul');
-        $subList.toggle();
-        return $target.toggleClass("toggled");
-      };
+  }).call(this);
 
-      return WillowLeaf;
+  WillowLeaf = (function() {
+    function WillowLeaf() {
+      this.toggle = __bind(this.toggle, this);
+    }
 
-    })();
-    return $.fn.willowTree = WillowTree.init;
-  })(jQuery);
+    WillowLeaf.prototype.toggle = function(event) {
+      var $parentListElement, $subList, $target, target;
+      target = event.target;
+      $target = $(target);
+      $parentListElement = $target.parent('li');
+      $subList = $parentListElement.children('ul');
+      $subList.toggle();
+      return $target.toggleClass("toggled");
+    };
+
+    return WillowLeaf;
+
+  })();
+
+  $.fn.willowTree = WillowTree.init;
 
 }).call(this);
